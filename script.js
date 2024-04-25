8574e88f4c155aaa3f4521f0a44309032a66131f
igor fernandes12b <igor.fernandes.jogos@gmail.com>
2024-04-25 03:12:23 +0000

    final do js

---
+++ script.js
@@ -0,0 +1,12 @@
+let botoes = document.querySelectorAll(".botoes button")
+
+botoes.forEach(botao => {
+
+    botao.addEventListener("click", )
+})
+
+    function guardarNota(evento){
+    let nota = evento.target.InnerText
+
+    localStorage.setItem("nota", nota)
+}
